5f702c06b7911342411e4191c73e9ccc54650f3f
JP Pasion <johnpaulpasionbricks01@gmail.com>
2022-11-01 16:32:03 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '589035783022792',
+        clientSecret: 'e86638c49b69762e33ea0df92445d69d'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var router = express.Router();
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 const cors = require('./cors');
 
+var router = express.Router();
 router.use(bodyParser.json());
+
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find({}, (err, users) => {
     if (err) {
@@ -67,6 +68,15 @@ router.get('/logout', cors.corsWithOptions, (req, res) => {
     var err = new Error('You are not logged in!');
     err.status = 403;
     next(err);
+  }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
   }
 });
 
